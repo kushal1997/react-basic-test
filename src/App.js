39531fda9a2d681115c414df07151fef408f1387
kushal1997 <kushalrao103@gmail.com>
2023-03-21 15:36:17 +0530

    learned about components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      App
     </div>
   );
 }
 
-export default App;
+
+
+
+
+// CLASS BASED COMPONENT USING --> rcc
+// import React, { Component } from 'react'
+
+// export default class App extends Component {
+//   render() {
+//     return (
+//       <div>App</div>
+//     )
+//   }
+// }
+
+
+// FUNCTION BASED COMPONENT
+
+// export default function App() {
+//   return (
+//     <div >
+//       App
+//     </div>
+//   );
+// }
+
+// FUNCTION BASED COMPONENT USING --> rafc
+
+// import React from 'react'
+// export const App = () => {
+//   return (
+//     <div>App</div>
+//   )
+// }
+
