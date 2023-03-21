20cca00f568a607e06968b2b62e33476e264b821
kushal1997 <kushalrao103@gmail.com>
2023-03-21 16:07:28 +0530

    learned basic JSX

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
 
 export default function App() {
+  const description ="Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea, iusto.";
   return (
-    <div >
-      App
-    </div>
+    // <div >
+    //   <h1>Kushal</h1>
+    //   <h2>Rao</h2>
+    // </div>
+
+    //WE SHOULD USE FRAGMENTS TO WRITE JSX
+    <>
+      <h1 className='active'>Kushal</h1>
+      <h2>Rao</h2>
+      <br />
+      {/* INLINE DYNAMIC CONTENT */}
+      <p>{description}</p> 
+    </>
   );
 }
 
